bd432b58239e9a3a4ac79b8058c3ab9ddc7a859f
Stas Zaremba <stanislav-zaremba@yandex.ru>
2023-01-11 01:25:29 +0300

    Isprawleno

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,30 @@
-
 const page = document.querySelector('.page');
 const content = page.querySelector('.content');
 const popupElement = content.querySelector('.popup');
 const profile = content.querySelector('.profile');
-
 const elements = content.querySelector('.elements');
 const cardElement = elements.querySelectorAll('.element');
 const elementImg = elements.querySelectorAll('.element__group');
 const contentElement = elements.querySelectorAll('.element__card');
-let titlElement = elements.querySelectorAll('.element__titl');
+let titleElement = elements.querySelectorAll('.element__title');
 
 // Пока я не нашел, как повесить обработчик событий на все лайки
 let likeElement = elements.querySelector('.element__like');
 
-let submitForm = content.querySelector('.submit-buttom');
+let submitForm = content.querySelector('.popup__submit-buttom');
 let profileButton = content.querySelector('.profile__edit-button');
 
 function clickButton() {
+    let userText = profile.querySelector('.profile__info-text');
+    let nameInput = popupElement.querySelector('.popup__input_type_neim');
     popupElement.classList.add('popup_opened');
+    nameInput.value = `${userName.textContent}`;
+    jobInput.value = `${userText.textContent}`;
 }
 
 profileButton.addEventListener('click', clickButton);
 
-const closeForm = content.querySelector('.close-icon');
+const closeForm = content.querySelector('.popup__close-icon');
 function formClose() {
     popupElement.classList.remove('popup_opened');
 }
@@ -30,22 +32,20 @@ function formClose() {
 closeForm.addEventListener('click', formClose);
 
 const form = content.querySelector('form');
-let userName = profile.querySelector('.profile-info__name');
-let userText = profile.querySelector('.profile-info__text');
-let nameInput = popupElement.querySelector('.popup__neim');
-let jobInput = popupElement.querySelector('.popup__profil');
+let userName = profile.querySelector('.profile__info-name');
+let userText = profile.querySelector('.profile__info-text');
+let nameInput = popupElement.querySelector('.popup__input_type_neim');
+let jobInput = popupElement.querySelector('.popup__input_type_profil');
 function handleFormSubmit(evt) {
     evt.preventDefault();
     userName.textContent = `${nameInput.value}`;
     userText.textContent = `${jobInput.value}`;
+    popupElement.classList.remove('popup_opened');
 }
 
 form.addEventListener('submit', handleFormSubmit);
 // Пока работает на первой кнопке
 function clickLike() {
-    likeElement.classList.toggle('element__like_activ');
-}   
-    likeElement.addEventListener('click', clickLike);
-//Надо двигаться дальше
-//С Новым годом и Рождеством. 
-//Здоровья Вам крепкого!!!
+    likeElement.classList.toggle('element__like_active');
+}
+likeElement.addEventListener('click', clickLike);
