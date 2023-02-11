0d51ca96f21b78ba6cacba7807fa650032361aed
Stas Zaremba <stanislav-zaremba@yandex.ru>
2023-02-11 15:56:14 +0300

    Добавлены кнопки на карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,13 @@
 const page = document.querySelector('.page');
 const content = page.querySelector('.content');
 const profile = content.querySelector('.profile');
-const elements = content.querySelector('.elements');//Блок карточек мест
 
-const cardElement = elements.querySelectorAll('.element');//Карточка мест
+const popupElement = content.querySelector('.popup');//Попап
+const popupNewItem = content.querySelector('.popup__item-conteiner');//Форма добавления карточек
+const profileForm = content.querySelector('.popup__container');// Форма редактироания профиля
+
+//const elements = content.querySelector('.elements');//Блок карточек мест
+//const cardElement = elements.querySelectorAll('.element');
 
 const elemCont = [
     {
@@ -30,76 +34,46 @@ const elemCont = [
         link: './image/Бамбуковыйлес.jpg',
         name: 'Бамбуковый лес',
     },
-    {
-        link: '',
-        name: '',
-    },
-    {
-        link: '',
-        name: '',
-    },
-    {
-        link: '',
-        name: '',
-    },
 ];
-/*
-const newElementName = elemCont.map(function (el) {
-    return el.name;
-});
-const newElementLink = elemCont.map(function (el) {
-    return el.link;
-});
-*/
-//console.log(userCard);
 
-//console.log(onlineUser);
-//console.log(elemCont.firstElementChild);
+//const titleElement = elements.querySelectorAll('.element__title');//Название карточки мест
+//const elementImg = elements.querySelectorAll('.element__group');//Фото карточки
 
+const profileAddButton = content.querySelector('.profile__add-button');//Кнопка добавления карточки
+const cardSec = content.querySelector('.elements');//нов
+const cardTemplate = content.querySelector('#element');
+console.log();
 
-const titleElement = elements.querySelectorAll('.element__title');//Название карточки мест
-const elementImg = elements.querySelectorAll('.element__group');//Фото карточки
+const creatTask = elemCont.forEach((elem) => {
+    const onlineUser = cardTemplate.content.querySelector('.element').cloneNode(true);
 
-const profileAddButton = content.querySelector('.profile__add-button');//Кнопка добавления элемента(карточки)
-const userTitl = elements.querySelector('.element__title');
+    onlineUser.querySelector('.element__title').textContent = elem.name;
+    onlineUser.querySelector('.element__group').src = elem.link;
+    const likeButt = onlineUser.querySelectorAll('.element__like');
+    const buttomDelet = onlineUser.querySelectorAll('.element__delet');
 
-elemCont.forEach(function (element) {
-const onlineUser = content.querySelector('.elements');
-const cardTemplate = content.querySelector('#element').content;
-const userCard = cardTemplate.querySelector('.element').cloneNode(true);
-    userCard.querySelector('.element__title').textContent = element.name;
-    userCard.querySelector('.element__group').src = element.link;
-
-
-    onlineUser.prepend(userCard);
-    //console.log(onlineUser);
-});
-
-const onlineUser = content.querySelector('.elements');
-const cardTemplate = content.querySelector('#element').content;
-const userCard = cardTemplate.querySelector('.element').cloneNode(true);
-const cardArticle = onlineUser.querySelector('.element');
-const likeButt = onlineUser.querySelectorAll('.element__like');
-console.log(likeButt);
-
-const like = likeButt.forEach((likeButt) => {
-    likeButt.addEventListener('click', () => {
-        likeButt.classList.toggle('element__like_active');
+    const like = likeButt.forEach((likeButt) => {
+        likeButt.addEventListener('click', () => {
+            likeButt.classList.toggle('element__like_active');
+        });
     });
-    return likeButt;
-});
-/*
-const contentElement = elements.querySelectorAll('.element__card');//Подвал карточки
-console.log(contentElement);
-*/
 
+    const deletCard = buttomDelet.forEach((buttomDelet) => {
+        buttomDelet.addEventListener('click', () => {
+            const cardArticle = buttomDelet.closest('.element');
+            cardArticle.remove();
+        });
+    });
+
+    cardSec.prepend(onlineUser);
+    //return onlineUser;
+});
+
+
+
+//console.log(contentElement);
 const submitForm = content.querySelector('.popup__submit-buttom');
 const profileButton = content.querySelector('.profile__edit-button');
-
-const popupElement = content.querySelector('.popup');//Попап
-const popupNewItem = content.querySelector('.popup__item-conteiner');//Форма добавления карточек
-const profileForm = content.querySelector('.popup__container');// Форма редактироания профиля
-
 const popupAddCard = content.querySelector('.popup__add-card');
 const popupEditProfile = content.querySelector('.popup__edit-profile');
 
@@ -145,10 +119,5 @@ function handleFormSubmit(evt) {
     userText.textContent = `${jobInput.value}`;
     popupElement.classList.remove('popup_opened');
 }
+
 form.addEventListener('submit', handleFormSubmit);
-
-
-/*
-console.log(likeElement.parentElement);//Ищем родителя
-console.log(contentElement.children);//Ищем детей
-*/
